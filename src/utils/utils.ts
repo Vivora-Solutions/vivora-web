2bb477faaa05161e63da8ff002c64fc121458efd
NipunSGeeTH <sangeethnipun385@gmail.com>
2025-09-18 19:00:08 +0530

    fix

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,5 @@
 const getImagePrefix = () => {
-  return process.env.NODE_ENV === "production" ? "/" : "";
+  return process.env.NODE_ENV === "production" ? "" : "";
 };
 
 export { getImagePrefix };
