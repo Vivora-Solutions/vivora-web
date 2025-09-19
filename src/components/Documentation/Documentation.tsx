3321463ecb2397966d4127844a8ebeb08d715883
Dulitha Perera <dulitha.22@cse.mrt.ac.lk>
2025-09-19 15:16:15 +0530

    Add documentation component

---
+++ src/components/Documentation/Documentation.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Documentation = () => {
+  return (
+    <div className="container mx-auto lg:max-w-screen-xl px-4 py-20">
+      <h1 className="text-white text-40 font-medium mb-6">
+        Documentation
+      </h1>
+      <p className="text-muted text-18">
+        Documentation content goes here.
+      </p>
+    </div>
+  );
+};
